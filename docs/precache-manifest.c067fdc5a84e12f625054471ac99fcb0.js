bf505ce56e0c1ae2941be6426e34a528c2c4f520
Devin Ryan Riota <devinryanriota@gmail.com>
2020-08-01 23:15:21 +0700

    build app

---
+++ docs/precache-manifest.c067fdc5a84e12f625054471ac99fcb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff164eb62318d60840b4f3ca2a593330",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de58da7380e77286d3b4",
+    "url": "/static/css/main.1e4ba2a4.chunk.css"
+  },
+  {
+    "revision": "9d36df6ac900d1d49f27",
+    "url": "/static/js/2.c855a017.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c855a017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de58da7380e77286d3b4",
+    "url": "/static/js/main.aa82cdfd.chunk.js"
+  },
+  {
+    "revision": "03d20b5be15dd0d6ec52",
+    "url": "/static/js/runtime-main.efad42fb.js"
+  },
+  {
+    "revision": "79d6ed6664a39d9da688e53d5133866e",
+    "url": "/static/media/logo.79d6ed66.png"
+  }
+]);
