8cd81a8cd20bc68e66c5d2bc45ef736b437c71de
Clavin June <juneardoc@gmail.com>
2020-08-01 13:45:17 +0700

    move header to app and change fixed to sticky

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Header } from './components'
 import { Index, NotFound } from './pages'
 
 export const App: React.FC<{}> = () => {
   return (
     <Router>
+      <Header />
       <Switch>
         <Route exact path='/'>
           <Index />
